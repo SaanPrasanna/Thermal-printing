6c34b6db6c7948362176298be2557c4ca3fe493c
SaanPrasanna <30783645+SaanPrasanna@users.noreply.github.com>
2021-02-12 07:18:06 +0530

    UI Updated

--- CustomJson/Form1.Designer.cs
+++ CustomJson/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(56, 33);
+            this.button1.Location = new System.Drawing.Point(59, 33);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -44,7 +44,7 @@
             // 
             // CmdMysql
             // 
-            this.CmdMysql.Location = new System.Drawing.Point(56, 66);
+            this.CmdMysql.Location = new System.Drawing.Point(59, 66);
             this.CmdMysql.Name = "CmdMysql";
             this.CmdMysql.Size = new System.Drawing.Size(75, 23);
             this.CmdMysql.TabIndex = 1;
